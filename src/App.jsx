f232773df9fb8116895231fd30de4f2c7cc6b499
houssam-dev <developpeur.pro.ma@gmail.com>
2022-05-28 06:45:24 +0100

    feat: add bottom nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 
 import { useEffect, useRef, useState } from 'react';
-import { MdEmail, MdPhone } from "react-icons/md";
+import { MdEmail, MdPhone, MdViewInAr, MdHome, MdPerson, MdWork, MdOutlineCode, MdWeb, MdOutlineConstruction, MdContacts, MdChat } from "react-icons/md";
 
 function App() {
 
 	return (
-		<div className="h-screen max-w-[1024px] mx-auto border-2 border-orange-300">
+		<div className="h-screen max-w-[1024px] mx-auto min-h-[3000px] border-2 border-red-300">
 
-			<section className="container-box flex flex-col sm:flex-row gap-4 sm:max-w-full sm:justify-between">
-				<div className=' min-w-[200px] max-w-[320px] '>
+			{/* === PROFIL === */}
+
+			{/* <section className="container-box flex flex-col sm:flex-row gap-6 sm:max-w-full sm:justify-between">
+				<div className=' min-w-[200px] max-w-[250px]'>
 					<div className='container-ratio-square'>
 						<img className=' rounded-xl child-ratio' src="src/images/my-photo.png" alt="" />
 					</div>
 				</div>
 				<div className=' flex flex-col gap-6 '>
-					<div className='flex flex-col gap-4 sm:flex-row sm:justify-between'>
+					<div className='flex flex-col gap-6 sm:flex-row sm:justify-between'>
 						<div>
 							<h1 className='font-sans font-semibold text-2xl text-[#4f4f4f]'>Billy Pearson</h1>
 							<span className='font-sans font-medium text-lg text-[#828282]'>Front-end developer</span>
@@ -35,8 +37,47 @@ function App() {
 						Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.
 					</div>
 				</div>
-			</section>
+			</section> */}
 
+			{/* === FRONT END PROGRESS BARS === */}
+
+			<header className='flex items-center justify-between gap-4 border-2 border-orange-400 '>
+				<a href=""><MdViewInAr /></a>
+				<nav className='hidden'>
+					<a href="#">Home</a>
+					<a href="#">About Me</a>
+					<a href="#">Projects</a>
+					<a href="#">Contact Me</a>
+				</nav>
+			</header>
+
+			{/*  MdHome, MdPerson, MdWork, MdOutlineCode, MdOutlineConstruction */}
+			<nav className='flex justify-evenly fixed bottom-0 left-0 right-0 bg-slate-500 py-2 m-1 rounded-xl'>
+				<a className='flex flex-col items-center gap-2 text-xs font-sans font-bold ' href="#">
+					<div className='rounded-full bg-blue-300 p-2'>
+						<MdHome size={24} />
+					</div>
+					<span>Home</span>
+				</a>
+				<a className='flex flex-col items-center gap-2 text-xs font-sans font-bold ' href="#">
+					<div className='rounded-full bg-blue-300 p-2'>
+						<MdPerson size={24} />
+					</div>
+					<span>About Me</span>
+				</a>
+				<a className='flex flex-col items-center gap-2 text-xs font-sans font-bold ' href="#">
+					<div className='rounded-full bg-blue-300 p-2'>
+						<MdOutlineConstruction size={24} />
+					</div>
+					<span>Projects</span>
+				</a>
+				<a className='flex flex-col items-center gap-2 text-xs font-sans font-bold ' href="#">
+					<div className='rounded-full bg-blue-300 p-2'>
+						<MdChat size={24} />
+					</div>
+					<span>Contact Me</span>
+				</a>
+			</nav>
 
 
 		</div>
